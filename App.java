a14a4c1d5503abae76ecdd37c8a6f6aa407febaf
PrajwalThorat <87720694+PrajwalThorat@users.noreply.github.com>
2021-08-14 18:36:23 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            SalesAnalyzer salesAnalyzer = new SalesAnalyzer("./data/purchase_details.csv");  
+            salesAnalyzer.analyse();
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not availabe");
+        }catch(IOException e){
+            System.out.println("File is Not Accessible");
+        }
+        
+    }
+    
+}
